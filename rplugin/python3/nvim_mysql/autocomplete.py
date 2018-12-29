0b95265dfa7e1896ac5a965361fc8d1d0e37839b
Joe Boyd <josefuboyd@gmail.com>
2018-12-29 14:31:01 -0500

    Improve autocomplete...
    Autocompletion now works in subqueries and queries with unions. It is
    more robust in general.

--- rplugin/python3/nvim_mysql/autocomplete.py
+++ rplugin/python3/nvim_mysql/autocomplete.py
@@ -8,79 +8,158 @@ import nvim_mysql.util
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-
-def get_table_aliases(query):
-    parsed_query = sqlparse.parse(query)[0]
-    aliases = {}
-    for t in parsed_query.tokens:
-        if isinstance(t, sqlparse.sql.Identifier):
-            if t.has_alias():
-                full_name = '.'.join([t.get_parent_name() or '', t.get_real_name()])
-                aliases[t.get_alias()] = full_name
-    return aliases
+_SENTINEL = 'NVIM_MYSQL_SENTINEL'
 
 
-def get_namespace(line_segment):
-    """Extract the namespace (database name or table alias) from line_segment.
+def _findstart(line_segment):
+    """Find start of text to autocomplete.
 
-    >>> get_namespace("from Person.a")
-    'Person'
-    >>> get_namespace("where x.na")
-    'x'
-    >>> get_namespace("where x.name = y.n")
-    'y'
-    >>> get_namespace("where length(x.na")
-    'x'
-    >>> get_namespace("where length(x.name) = length(x2.n")
-    'x2'
+    >>> _findstart("where x.pers")
+    8
+    >>> _findstart("from d.person, d.na")
+    17
+    >>> _findstart("    and x.")
+    10
     """
-    end = line_segment.rindex('.')
-    start = -1
-    for char in ' (':
-        loc = line_segment.rfind(char)
-        if loc > start:
-            start = loc
-    if start == -1:
-        raise ValueError("Invalid location for autocomplete")
-    return line_segment[start+1:end]
+    if '.' in line_segment:
+        return line_segment.rindex('.') + 1
+    else:
+        return -1
+
+
+def _get_namespace_for_autocomplete(query, row, col):
+    """Get the namespace (database or table) that contains the autocomplete candidates."""
+    class _TraversalContext(object):
+        def __init__(self):
+            self.scopes = [{'bindings': {}, 'level': 0}]
+            self.rv = None
+
+        @property
+        def bindings(self):
+            return self.scopes[-1]['bindings']
+
+        @property
+        def level(self):
+            return self.scopes[-1]['level']
+
+        @staticmethod
+        def get_token_level(token):
+            level = 0
+            while token.parent is not None:
+                level += 1
+                token = token.parent
+            return level
+
+        @staticmethod
+        def is_select(token):
+            return token.ttype is sqlparse.tokens.DML and token.value.upper() == 'SELECT'
+
+        @staticmethod
+        def is_binding(token):
+            return isinstance(token, sqlparse.sql.Identifier) and token.has_alias()
+
+        @staticmethod
+        def is_sentinel(token):
+            return isinstance(token, sqlparse.sql.Identifier) and _SENTINEL in str(token)
+
+        def opens_scope(self, token):
+            return self.is_select(token)
+
+        def closes_scope(self, token):
+            level = self.get_token_level(token)
+            return (
+                (self.is_select(token) and level == self.level) or
+                (level < self.level)
+            )
+
+        def open_scope(self, token):
+            level = self.get_token_level(token)
+            if level > self.level:
+                self.scopes.append({'bindings': self.bindings.copy(), 'level': level})
+            else:
+                self.scopes.append({'bindings': {}, 'level': level})
+
+        def close_scope(self):
+            target_name = self.scopes[-1].get('target_name', None)
+            if target_name is not None:
+                if target_name in self.bindings:
+                    self.rv = self.bindings[target_name]
+                else:
+                    self.rv = target_name
+            del self.scopes[-1]
+
+        def add_binding(self, token):
+            if token.get_parent_name() is not None:
+                self.bindings[token.get_alias()] = token.get_parent_name() + '.' + token.get_real_name()
+            else:
+                self.bindings[token.get_alias()] = token.get_real_name()
+
+        def handle_sentinel(self, token):
+            self.scopes[-1]['target_name'] = token.get_parent_name()
+
+        def handle_token(self, token):
+            if self.closes_scope(token):
+                self.close_scope()
+            if self.opens_scope(token):
+                self.open_scope(token)
+            if self.is_binding(token):
+                self.add_binding(token)
+            if self.is_sentinel(token):
+                self.handle_sentinel(token)
+
+        def close_all_scopes(self):
+            while self.scopes:
+                self.close_scope()
+
+
+    def _traverse(token, ctx):
+        ctx.handle_token(token)
+        if hasattr(token, 'tokens'):
+            for t in token.tokens:
+                _traverse(t, ctx)
+
+    # Insert sentinel to mark where we are autocompleting.
+    lines = query.splitlines()
+    lines[row] = lines[row][:col] + _SENTINEL + lines[row][col:]
+    query = '\n'.join(lines)
+
+    # Parse query and traverse.
+    tree = sqlparse.parse(query)[0]
+    ctx = _TraversalContext()
+    _traverse(tree, ctx)
+    ctx.close_all_scopes()
+    return ctx.rv
+
+
+def _complete(line_segment, base, vim, cursor):
+    logger.debug('autocomplete: base: "{}"'.format(base))
+    logger.debug('autocomplete: line segment is "{}"'.format(line_segment))
+
+    row, col = vim.current.window.cursor[0] - 1, vim.current.window.cursor[1]
+    query = nvim_mysql.util.get_query_under_cursor(vim.current.buffer, row, col)
+    namespace = _get_namespace_for_autocomplete(query, row, col)
+    logger.debug('autocomplete: namespace is "{}"'.format(namespace))
+
+    cursor.execute("show databases")
+    databases = [r[0] for r in cursor.fetchall()]
+    if namespace in databases:
+        # Assume table
+        logger.debug("autocomplete: assuming we're completing a TABLE")
+        cursor.execute("show tables from `{}`".format(namespace))
+        words = [r[0] for r in cursor.fetchall() if r[0].lower().startswith(base.lower())]
+    else:
+        # Assume column
+        logger.debug("autocomplete: assuming we're completing a COLUMN")
+        cursor.execute("describe {}".format(namespace))
+        words = [r[0] for r in cursor.fetchall() if r[0].lower().startswith(base.lower())]
+
+    return [{'word': w, 'icase': 1} for w in words]
 
 
 def complete(findstart, base, vim, cursor):
-    cursor.execute("show databases")
-    databases = [r[0] for r in cursor.fetchall()]
     col = vim.current.window.cursor[1]
     line_segment = vim.current.line[:col]
     if findstart:
-        if '.' in line_segment:
-            return line_segment.rindex('.') + 1
-        else:
-            return -1
+        return _findstart(line_segment)
     else:
-        logger.debug('autocomplete: base: "{}"'.format(base))
-        logger.debug('autocomplete: line segment is "{}"'.format(line_segment))
-        namespace = get_namespace(line_segment)
-        logger.debug('autocomplete: namespace is "{}"'.format(namespace))
-        if namespace in databases:
-            # Assume table
-            logger.debug("autocomplete: assuming we're completing a TABLE")
-            cursor.execute("show tables from `{}`".format(namespace))
-            words = [r[0] for r in cursor.fetchall() if r[0].lower().startswith(base.lower())]
-        else:
-            # Assume column
-            logger.debug("autocomplete: assuming we're completing a COLUMN")
-            query = nvim_mysql.util.get_query_under_cursor(
-                vim.current.buffer,
-                vim.current.window.cursor[0] - 1,
-                vim.current.window.cursor[1]
-            )
-            aliases = get_table_aliases(query)
-            logger.debug("autocomplete: query aliases: {}".format(aliases))
-            if namespace in aliases:
-                table = aliases[namespace]
-            else:
-                table = namespace
-            logger.debug("autocomplete: table: {}".format(table))
-            cursor.execute("describe {}".format(table))
-            words = [r[0] for r in cursor.fetchall() if r[0].lower().startswith(base.lower())]
-
-        return [{'word': w, 'icase': 1} for w in words]
+        return _complete(line_segment, base, vim, cursor)
